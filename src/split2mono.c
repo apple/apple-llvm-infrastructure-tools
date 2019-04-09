2421755707a4bdba1d2b887a7f211fe72a557150
Duncan P. N. Exon Smith <dexonsmith@apple.com>
2019-04-08 23:41:33 -0400

    Start building sqlite-based split2mono
    This is a draft of `split2mono-insert`, for new adding commits.  Similar
    to svn2git-insert, but doesn't know the number of records.
    
    - Adds records a few at a time to amortize the cost of constructing a
      transaction.
    - Create tables on creation.
    
    Soon, a `split2mono-upstream` alias for merging upstream databases will
    follow.  The plan is:
    
    - Create tables on creation.
    - Take an upstream database (tied to a remote) and a name for it.
    - Insert any new commits from the upstream.
        - Error on collision? (same split, different mono)
    - Update upstreams.
        - Error if upstream has fewer transitive upstreams than in the last
          sync.
        - Error if upstream an has fewer commits than in the last sync.
        - Add any new transitive upstreams.

---
+++ src/split2mono.c
@@ -0,0 +1,150 @@
+// split2mono.c
+//
+// build: clang -x c -std=gnu11 -O2 -lsqlite3
+// alias: split2mono-insert
+#include <fcntl.h>
+#include <stdio.h>
+#include <string.h>
+
+int show_progress(int total) {
+  return fprintf(stderr, "   %9d commits mapped\n", total) < 0;
+}
+int error(const char *msg) {
+  fprintf(stderr, "error: %s\n", msg);
+  return 1;
+}
+int usage(const char *msg, int argc, const char *argv[]) {
+  error(msg);
+  fprintf(stderr, "usage: %s <db>\n", argv[0]);
+  return 1;
+}
+
+#define SQLITE_OPEN_READWRITE 0x00000002
+#define SQLITE_OPEN_CREATE 0x00000004
+
+typedef void *sqlite3_handle;
+int sqlite3_open_v2(const char *filename, sqlite3_handle *db, int flags,
+                    const char *vfs);
+int sqlite3_close_v2(sqlite3_handle db);
+void sqlite3_free(void *mem);
+
+
+int sqlite3_exec(sqlite3_handle db, const char *sql,
+                 int (*callback)(void *, int, char **, char **), void *context,
+                 char **error);
+int execute(sqlite3_handle db, const char *command) {
+  char *error;
+  if (!sqlite3_exec(db, command, 0, 0, &error))
+    return 0;
+  sqlite3_free(error);
+  return 1;
+}
+
+int create_tables(sqlite3_handle db) {
+  if (!execute(db, "CREATE TABLE upstream_dbs ("
+                   "id INT PRIMARY KEY NOT NULL,"
+                   "name TEXT UNIQUE NOT NULL,"
+                   "upstream_dbs_count INT NOT NULL,"
+                   "split2mono_count INT NOT NULL);"))
+    return error("could not create 'upstreams' table");
+  if (!execute(db, "CREATE TABLE split2mono ("
+                   "id INT PRIMARY KEY NOT NULL,"
+                   "split CHAR(40) UNIQUE NOT NULL,"
+                   "mono CHAR(40) NOT NULL);"))
+    return error("could not create 'split2mono' table");
+  return 0;
+}
+
+int initialize_db(sqlite3_handle *db, const char *filename, int argc,
+                  const char *argv[]) {
+  if (!sqlite3_open_v2(filename, db, SQLITE_OPEN_READWRITE, 0))
+    return 0;
+  sqlite3_close_v2(*db);
+
+  if (sqlite3_open_v2(filename, db,
+                      SQLITE_OPEN_READWRITE | SQLITE_OPEN_CREATE, 0))
+    return usage("can't open <db>", argc, argv);
+  if (!create_tables(*db))
+    return 0;
+
+  sqlite3_close_v2(*db);
+  *db = 0;
+  if (remove(filename))
+    return error("can't remove <db> (after creating it!)");
+  return 1;
+}
+
+int insert_commits(sqlite3_handle db) {
+  const char prefix[] = "INSERT INTO split2mono(split,mono)VALUES(\"";
+  const char record_delim[] = "\"),(\"";
+  const char value_delim[] = "\",\"";
+  const char suffix[] = "\");";
+  char buffer[2048] = {0};
+  const int record_size = 80 + sizeof(value_delim) + sizeof(record_delim);
+  const int max_records =
+      (sizeof(buffer) - 1 - sizeof(prefix) - sizeof(suffix)) /
+      record_size;
+  char *record_start =
+      buffer + snprintf(buffer, sizeof(prefix), "%s", prefix);
+  char *current = record_start;
+
+  char split[41];
+  char mono[41];
+  int n = 0;
+  int progress = 0;
+  while (scanf("%s %s", split, mono) == 2) {
+    current += snprintf(current, record_size, "%s%s%s", split, value_delim,
+                        mono);
+
+    if (n < max_records) {
+      current += snprintf(current, sizeof(record_delim), "%s", record_delim);
+      n = 0;
+      continue;
+    }
+
+    current += snprintf(current, sizeof(suffix), "%s", suffix);
+    *current = '\0';
+    if (execute(db, buffer))
+      return 1;
+    current = record_start;
+
+    if (n - progress > 5000) {
+      progress = n;
+      if (show_progress(n))
+        return 1;
+    }
+  }
+  if (!n)
+    return 0;
+  current += snprintf(current, sizeof(suffix), "%s", suffix);
+  *current = '\0';
+  return execute(db, buffer);
+}
+
+int split2mono_insert(sqlite3_handle *db, int argc, const char *argv[]) {
+  if (argc < 2)
+    return usage("missing <db>", argc, argv);
+  const char *filename = argv[1];
+
+  if (initialize_db(db, filename, argc, argv))
+    return 1;
+  return insert_commits(*db);
+}
+
+int main(int argc, const char *argv[]) {
+  sqlite3_handle db = 0;
+  const char *name = argv[0];
+  char *slash = strrchr(name, '/');
+  if (slash)
+    name = slash + 1;
+  int status;
+  if (strcmp(name, "split2mono-insert")) {
+    status = split2mono_insert(&db, argc, argv);
+  } else {
+    fprintf(stderr, "unrecognized invocation name '%s'", name);
+    return 1;
+  }
+  if (db)
+    sqlite3_close_v2(db);
+  return status;
+}
