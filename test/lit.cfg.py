5d7a31f2b9fa4f3e989be9890f45656aaea47ae3
Duncan P. N. Exon Smith <dexonsmith@apple.com>
2019-05-07 14:17:53 -0700

    Avoid the need for multiple lit.cfg.py
    Consolidate into a single top-level lit.cfg.py.
    - `make check-lit` runs all the lit tests
    - `make check-X` runs the lit tests in the `X/` subdirectory

--- test/lit.cfg.py
+++ test/lit.cfg.py
@@ -2,19 +2,20 @@ import os, sys
 import lit.formats
 
 config.name = 'not'
-config.test_format = lit.formats.ShTest()
+config.test_format = lit.formats.ShTest(execute_external=False)
 
 # suffixes: A list of file extensions to treat as test files.
 config.suffixes = ['.test']
 
-# test_exec_root: The root path where tests should be run.
-config.test_exec_root = os.path.join(os.path.dirname(__file__), '.lit_test_output')
+# test_source_root: where to run the tests from
+config.test_source_root = os.path.dirname(__file__)
 
-not_test_source_root = os.path.dirname(__file__)
-test_source_root = os.path.dirname(not_test_source_root)
-src_root = os.path.dirname(test_source_root)
+# test_exec_root: The root path where tests should be run.
+config.test_exec_root = os.path.join(config.test_source_root, 'Run.lit')
+
+src_root = os.path.dirname(config.test_source_root)
 bindir = os.path.join(src_root, 'bin')
-testbindir = os.path.join(test_source_root, 'bin')
+testbindir = os.path.join(config.test_source_root, 'bin')
 
 # Update the path.
 path = config.environment['PATH']
